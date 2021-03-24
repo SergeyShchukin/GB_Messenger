32bd29de1a7f0c496f8e77ffd538aed9506e8271
SergeyShchukin <shchukin.s.a@mail.ru>
2021-03-25 00:36:42 +0300

    l1

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import Messages from "./messages.js";
+
+const App = () => {
+    return (
+        <>
+        <h1>Hello React!</h1>
+        <Messages />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/messages.js
@@ -0,0 +1,14 @@
+﻿import React, { useState } from "react";
+
+const Messages = () => {
+    const [arrMessages, setState] = useState(["Привет!", "Как дела?"]);
+
+    return (
+        <>
+            {arrMessages.map((text, index) => <div key={index}>{text}</div>)}
+            <button onClick={() => setState(arrMessages => [...arrMessages, "Нормально"])}>Отправить сообщение</button>
+        </>
+    );
+};
+
+export default Messages;
