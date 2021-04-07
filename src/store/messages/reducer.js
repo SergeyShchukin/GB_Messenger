fb4ff37eb73401ed43173fc77a37495fd35410fb
SergeyShchukin <shchukin.s.a@mail.ru>
2021-04-07 13:21:06 +0300

    l5_1

--- src/store/messages/reducer.js
+++ src/store/messages/reducer.js
@@ -3,21 +3,13 @@
 export const messagesReducer = (state = {}, action) => {
 	switch (action.type) {
 		case ADD_MESSAGE: {
-			// проверка что в чате уже есть сообщения. Без нее не работает конструкция ...state[action.payload.chatId]
-			if (state[action.payload.chatId]) {
-				return {
-					...state,
-					[action.payload.chatId]: [
-						...state[action.payload.chatId],
-						{ text: action.payload.text, author: action.payload.author, id: `${action.payload.chatId}_${state[action.payload.chatId].length}` },
-					],
-				};
-			} else {
-				return {
-					...state,
-					[action.payload.chatId]: [{ text: action.payload.text, author: action.payload.author, id: `${action.payload.chatId}_0` }],
-				};
-			}
+			return {
+				...state,
+				[action.payload.chatId]: [
+					...(state[action.payload.chatId] || []),
+					{ text: action.payload.text, author: action.payload.author, id: `${action.payload.chatId}_${state[action.payload.chatId]?.length || 0}` },
+				],
+			};
 		}
 
 		default:
